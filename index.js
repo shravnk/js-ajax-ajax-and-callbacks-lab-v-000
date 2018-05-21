43ffd85c5fb05b92941e7531027e42444064ebfe
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-21 02:41:10 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-  const req = new XMLHttpRequest()
   const searchTerms = document.getElementById('searchTerms').value
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response) {
     $("#results").html(showRepositories(response))
+  }).fail(error => {
+    displayError()
   })
 }
 
@@ -14,14 +15,13 @@ function showRepositories(response) {
   `<ul>${response.items.map(repo => {
     return (
       `<li>
-      <h2><a href="${repo.html_url}">${repo.name}</a></h2>
+      <h2>${repo.name}</h2>
+      <a href="${repo.html_url}">${repo.html_url}</a>
       <h4>Created by: ${repo.owner.login}</h4>
       <img src="${repo.owner.avatar_url}" height="32" width="32">
       <a href="#" data-owner="${repo.owner.login}" data-repository="${repo.name}" onClick="showCommits(this)">Commits</a>
       </li>`)}).join('')}
       </ul>`
-
-
   return repoList
 }
 
